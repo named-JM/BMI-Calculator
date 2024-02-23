84b288acafed66edb309a140dc912c3c14087350
Echel Cags <joannacaguco@gmail.com>
2024-02-23 17:02:28 +0800

    initial commit

---
+++ app/src/main/java/com/example/myapp/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.myapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        EditText editTextWeight = findViewById(R.id.weight);
+        EditText editTextHeight = findViewById(R.id.height);
+        Button button = findViewById(R.id.btnSubmit);
+        TextView textView = findViewById(R.id.result);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                float height = Float.parseFloat(String.valueOf(editTextHeight.getText())) / 100;
+                float weight = Float.parseFloat(String.valueOf(editTextWeight.getText()));
+                float bmi = weight / (height *height);
+                textView.setText(String.valueOf(bmi));
+
+
+            }
+        });
+    }
+}
